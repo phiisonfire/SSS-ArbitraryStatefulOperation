bfa5e0b8b06028ff8dc55a06dd0520538e1b7e50
Phi Nguyen Laptop <phinguyen@laptop>
2024-08-16 16:44:20 +0700

    implemented arbitrary-stateful-processing, need to test with data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
 package de.phinguyen.sparkstructuredstreaming
 
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, OutputMode}
+
+import java.time.{Duration, Instant}
+import scala.collection.mutable.ListBuffer
+
 object Main {
   /* Transaction count within the last 5 minutes
 
@@ -22,7 +28,114 @@ object Main {
                            eventTimestamp: java.time.Instant,
                            isTimeout: Boolean)
 
-  
+  // Support functions
+  def addNewRecords(transactionList: List[InputRow], state: PurchaseCountState) = {
+    val latestTimestamp = if (transactionList.nonEmpty) {
+      transactionList.map(row => row.transactionTimestamp).max
+    } else {
+      state.latestTimeStamp
+    }
+    new PurchaseCountState(latestTimestamp, state.currentPurchases ::: transactionList)
+  }
+
+  def removeExpiredRecords(latestTimestamp: java.time.Instant,state: PurchaseCountState) = {
+    // val latestTimestamp = state.latestTimeStamp
+    val expirationDuration = Duration.ofMinutes(5)
+    val filterPurchases = state.currentPurchases.filter( purchase =>
+      purchase.transactionTimestamp.isAfter(latestTimestamp.minus(expirationDuration))
+    )
+    new PurchaseCountState(latestTimestamp, filterPurchases)
+  }
+
+  // Function (with a specific set of parameters) that will be executed by `flatMapGroupWithState`
+  // 1st parameter ~ grouping key = the column from the streaming DataFrame being group on
+
+  def updateState(userId: Long,
+                  values: Iterator[InputRow],
+                  state: GroupState[PurchaseCountState]
+                 ): Iterator[PurchaseCount] = {
+    val purchaseCounts = ListBuffer[PurchaseCount]()
+    if (!state.hasTimedOut) { // login when new records received within the timeout period
+      val transactionList = new ListBuffer[InputRow]()
+      transactionList ++= values
+      var prevState = state.getOption.getOrElse {
+        // initialization logic - when received the first record of the key
+        val firstTransactionTimestamp = transactionList.head.transactionTimestamp
+        new PurchaseCountState(firstTransactionTimestamp, List[InputRow]())
+      }
+      // steady-state logic - when received new records for existing key
+      val stateWithNewRecords = addNewRecords(transactionList.toList, prevState)
+      val stateWithRecordsRemoved = removeExpiredRecords(stateWithNewRecords.latestTimeStamp, stateWithNewRecords)
+      val output = new PurchaseCount(userId,
+        stateWithRecordsRemoved.currentPurchases.size,
+        stateWithRecordsRemoved.latestTimeStamp,
+        false
+      )
+      purchaseCounts.append(output)
+      state.update(stateWithRecordsRemoved)
+      // reset the timeout by extending 30 seconds from the latest event timestamp
+      state.setTimeoutTimestamp(stateWithRecordsRemoved.latestTimeStamp.toEpochMilli(), "30 seconds")
+    } else { // logic when no events received within the timeout period
+      // Timeout logic will not have an incoming set of new records, so no need to access the input Iterator
+      val prevState = state.get
+      val newTimestamp = Instant.now
+      val stateWithRecordsRemoved = removeExpiredRecords(newTimestamp, prevState)
+      val output = new PurchaseCount(userId = userId,
+        purchaseCount = stateWithRecordsRemoved.currentPurchases.size,
+        eventTimestamp = stateWithRecordsRemoved.latestTimeStamp,
+        isTimeout = true)
+      purchaseCounts.append(output)
+      state.update(stateWithRecordsRemoved)
+      state.setTimeoutTimestamp(stateWithRecordsRemoved.latestTimeStamp.toEpochMilli(), "30 seconds")
+    }
+    purchaseCounts.iterator
+    // time-out logic - when no records have been received for a key (for a period of time)
+    // When we need a time out?
+    // - If results need to be output or the state needs to change even if no records have been received
+  }
+
+  def main(args: Array[String]) = {
+    // Import implicits for Encoders
+    // Spark automatically creates Encoder for case classes
+    // in this situation, Spark creates Encoder[InputRow]
+    // Encoders are necessary because they define how the JVM objects are serialized and deserialzed when working with DataFrames and DataSets
+    import spark.implicits._
+
+    val spark = SparkSession.builder()
+      .master("local[4]")
+      .appName("SSS-ArbitraryStatefulProcessing")
+      .getOrCreate()
+
+    val kafkaInputDS = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "user-transaction-topic")
+      .option("startingOffsets", "latest")
+      .load() // this load the whole kafka messages and their metadata
+      .selectExpr("userId", "cast(transactionTimestamp as timestamp) transactionTimestamp")
+      .as[InputRow]
+
+    val resultDS: Dataset[PurchaseCount] = kafkaInputDS
+      .withWatermark("transactionTimestamp", "30 seconds")
+      .groupByKey(_.userId)
+      .flatMapGroupsWithState(OutputMode.Append(), GroupStateTimeout.EventTimeTimeout())(updateState)
+
+    // recommend convert resultDS to DataFrame
+    // sink: Kafka, Delta, foreachBatch
+
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
